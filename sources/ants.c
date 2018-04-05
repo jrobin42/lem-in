a632227356bc86eabf2480f0f1f5e1d39456f2c2
Jeanne ROBIN <jrobin@e2r11p23.42.fr>
2018-04-05 07:09:50 +0200

    add enought space for the \0 in lstnew

--- sources/ants.c
+++ sources/ants.c
@@ -6,7 +6,7 @@
 /*   By: jrobin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/13 14:03:36 by jrobin            #+#    #+#             */
-/*   Updated: 2018/04/05 05:56:28 by jrobin           ###   ########.fr       */
+/*   Updated: 2018/04/05 06:50:06 by jrobin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@
 int		get_nb_ants(char **line, t_lemin *l)
 {
 	if (get_next_line(0, line) < 1)
-	{
 		return (FAILURE);
-	}
 	if (ft_isint(*line))
 	{
 		if ((l->nb_ants = ft_atoi(*line)) < 1)
@@ -26,7 +24,7 @@ int		get_nb_ants(char **line, t_lemin *l)
 			free(*line);
 			return (FAILURE);
 		}
-		l->to_print = ft_lstnew(*line, ft_strlen(*line));
+		l->to_print = ft_lstnew(*line, ft_strlen(*line) + 1);
 	}
 	else
 	{
