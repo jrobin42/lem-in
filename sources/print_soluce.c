68f783416460ef723d21c40e18cbef3cacc39778
Jeanne ROBIN <jrobin@e2r9p8.42.fr>
2018-04-10 05:17:30 +0200

    the multipath is print <3

--- sources/print_soluce.c
+++ sources/print_soluce.c
@@ -6,7 +6,7 @@
 /*   By: jrobin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/06 05:53:37 by jrobin            #+#    #+#             */
-/*   Updated: 2018/04/10 04:44:50 by jrobin           ###   ########.fr       */
+/*   Updated: 2018/04/10 05:16:34 by jrobin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,18 @@ static void			print_input(t_list *to_print, int step)
 	}
 }
 
-static void			print_curr_state(t_room **r, int **t, int *len_path, int nb_paths, int index)
+static void			print_curr_state(t_room **r, int *t, int len_path, int nb_paths)
 {
 	int		i;
 
 		i = 0;
-		while (i < len_path[index])
+		while (i < len_path)
 		{
-			if (r[t[index][i]]->ant)
-				ft_printf("L%d-%s ", r[t[index][i]]->ant, r[t[index][i]]->name);
+			if (r[t[i]]->ant)
+				ft_printf("L%d-%s ", r[t[i]]->ant, r[t[i]]->name);
 			++i;
 		}
 		(void)nb_paths;
-/*	index == nb_paths - 1 ?*/ ft_printf("\n") /*: 0*/;
 }
 
 void				usefull_paths(int **ants_for_each, int nb_ants, int *len_p,
@@ -76,45 +75,59 @@ void				usefull_paths(int **ants_for_each, int nb_ants, int *len_p,
 	}
 }
 
+static void			pull_ant(int nb_ants, t_room **r, int *path, int len_p)
+{
+	int		i;
+	int		nb;
+
+	nb = 0;
+//	while (nb < nb_ants)
+//	{
+		i = -1;
+		while (++i < len_p)
+			if (i == 0 && r[path[1]]->ant > 0 && ++nb)
+			{
+				r[path[i]]->ant = r[path[i + 1]]->ant;
+				r[path[i + 1]]->ant = 0;
+			}
+			else if (i == len_p - 1 && r[path[i]]->ant == 0 && r[0]->ant > 0)
+			{
+				r[path[i]]->ant = nb_ants - r[path[i + 1]]->ant + 1;
+				r[path[i + 1]]->ant -= 1;
+			}
+			else if (r[path[i]]->ant == 0 && r[path[i + 1]]->ant)
+			{
+				r[path[i]]->ant = r[path[i + 1]]->ant;
+				r[path[i + 1]]->ant = 0;
+			}
+//	}
+}
+
 void				print_soluce(int **paths, t_lemin *l, int nb_paths,
 		int *len_p)
 {
 	int		i;
 	int		index_p;
-	int		nb;
 	int		*ants_for_each;
 	t_room	**r;
 
 	index_p = 0;
 	r = l->rooms;
-	nb = 0;
 	print_input(l->to_print, l->step);
 	if ((ants_for_each = ft_memalloc(nb_paths * sizeof(int))) == NULL)
 		exit(-1);
 	if (nb_paths > 1)
 		usefull_paths(&ants_for_each, l->nb_ants, len_p, &nb_paths);
 	r[0]->ant = l->nb_ants;
-	while (nb < l->nb_ants)
+	while (r[l->nb_rooms - 1]->ant < l->nb_ants)
 	{
-		print_curr_state(r, paths, len_p, nb_paths, index_p);
-		i = -1;
-		while (++i < *len_p)
-			if (i == 0 && r[paths[index_p][1]]->ant > 0 && ++nb)
-			{
-				r[paths[index_p][i]]->ant = r[paths[index_p][i + 1]]->ant;
-				r[paths[index_p][i + 1]]->ant = 0;
-			}
-			else if (i == *len_p - 1 && r[paths[index_p][i]]->ant == 0 && r[0]->ant > 0)
-			{
-				r[paths[index_p][i]]->ant = l->nb_ants - r[paths[index_p][i + 1]]->ant + 1;
-				r[paths[index_p][i + 1]]->ant -= 1;
-			}
-			else if (r[paths[index_p][i]]->ant == 0 && r[paths[index_p][i + 1]]->ant)
-			{
-				r[paths[index_p][i]]->ant = r[paths[index_p][i + 1]]->ant;
-				r[paths[index_p][i + 1]]->ant = 0;
-			}
-		++i;
+		i = 0;
+		while (i < nb_paths)
+		{
+			pull_ant(l->nb_ants, r, paths[i], len_p[i]);
+			print_curr_state(r, paths[i], len_p[i], nb_paths);
+			++i;
+		}
+		ft_printf("\n");
 	}
-	print_curr_state(r, paths, len_p, nb_paths, index_p);
 }
